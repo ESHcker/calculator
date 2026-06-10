cb95a1daf27e23e0bdc548e3691db96e092a12c7
ESHcker <274484520+ESHcker@users.noreply.github.com>
2026-06-10 13:03:10 +0100

    Add function for display and update digits + result Add display

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
-let number1;
-let operator;
-let number2;
+let number1 = "";
+let operator = "";
+let number2 = "";
 
 function add(number1, number2){
-    return number1 + number2;
+    return Number(number1) + Number(number2);
 }
 
 function subtract(number1, number2){
-    return number1 - number2;
+    return Number(number1) - Number(number2);
 }
 
 function multiply(number1, number2){
-    return number1 * number2;
+    return Number(number1) * Number(number2);
 }
 
 function divide(number1, number2){
-    return number1 / number2;
+    return Number(number1) / Number(number2);
 }
 
-function operate(operator, number1, number2){
+function operate(){
+    let result = "";
     switch(operator){
-        case "+": return add(number1, number2);
-        case "-": return subtract(number1, number2);
-        case "x": return multiply(number1, number2);
-        case "/": return divide(number1, number2);
+        case "+": result += add(number1, number2); break;
+        case "-": result += subtract(number1, number2); break;
+        case "x": result += multiply(number1, number2); break;
+        case "/": result += divide(number1, number2); break;
     }
+    displayToUser("=" + result);
+}
+
+function updateDigit(number){
+    if(operator === ""){
+        number1+= number;
+    }else{
+        number2+= number;
+    }
+    displayToUser(number);
+}
+
+function updateOperator(operatorToUpdate){
+    operator = operatorToUpdate;
+    displayToUser(operator);
+}
+
+function displayToUser(elementToDisplay){
+    let displayToUse = document.getElementById("display");
+    displayToUse.textContent += elementToDisplay;
 }
