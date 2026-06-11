dc46985a1db182f2a4567033c0e0e2e027905677
ESHcker <274484520+ESHcker@users.noreply.github.com>
2026-06-11 12:38:48 +0100

    Add reset calculator function Modify displaytoUser for use in reset calculator function Modify updateOperaor only take in consideration first operator Modify updateDigit when get a result, reset calculator with new digit Modify operate control use a operator before second digit,reset calculator when operate Add new variable to control results

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let number1 = "";
 let operator = "";
 let number2 = "";
+let operateDoneBefore = false;
 
 function add(number1, number2){
     return Number(number1) + Number(number2);
@@ -19,31 +20,62 @@ function divide(number1, number2){
 }
 
 function operate(){
-    let result = "";
-    switch(operator){
-        case "+": result += add(number1, number2); break;
-        case "-": result += subtract(number1, number2); break;
-        case "x": result += multiply(number1, number2); break;
-        case "/": result += divide(number1, number2); break;
-    }
-    displayToUser("=" + result);
+    if(number1 != ""){
+        let result = "";
+        switch(operator){
+            case "+": result += add(number1, number2); break;
+            case "-": result += subtract(number1, number2); break;
+            case "x": result += multiply(number1, number2); break;
+            case "/": result += divide(number1, number2); break;
+        }
+        number1 = result;
+        operateDoneBefore = true;
+        resetCalculator("semi");
+    } 
 }
 
 function updateDigit(number){
-    if(operator === ""){
-        number1+= number;
+    if(operateDoneBefore === false){
+        if(operator === ""){
+            number1+= number;
+            displayToUser(number);
+        }else if(operator != ""){
+            number2+= number;
+            displayToUser(number);
+        }
     }else{
-        number2+= number;
+        resetCalculator("All");
+        updateDigit(number);
     }
-    displayToUser(number);
+    
 }
 
 function updateOperator(operatorToUpdate){
-    operator = operatorToUpdate;
-    displayToUser(operator);
+    if(operator === ""){
+        operator = operatorToUpdate;
+        displayToUser(operator);
+    }
 }
 
 function displayToUser(elementToDisplay){
     let displayToUse = document.getElementById("display");
-    displayToUse.textContent += elementToDisplay;
+    if(elementToDisplay == ""){    
+        displayToUse.textContent = elementToDisplay;
+    }else{
+        displayToUse.textContent += elementToDisplay;
+    }
 }
+
+function resetCalculator(typeOfClear){
+    operator = "";
+    number2 = "";
+    displayToUser("");
+    if(typeOfClear === "All"){
+        number1 = "";
+        operateDoneBefore = false;
+    }
+    if(typeOfClear === "semi"){
+        displayToUser(number1);
+    }
+}
+
